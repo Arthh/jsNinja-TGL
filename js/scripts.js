d3aac2cb2f137c7b2943bc6b27f4b5983e9cddc0
Arthh <arthur.ulhoa28@gmail.com>
2021-04-06 11:53:20 -0300

    feat: Game logic complete, starting list games on cart

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,8 @@
   var selectedGame;
   var $divListGames = getElement('[class=list-of-games]');
   var $divNumberGame = getElement('[class="games-list-numbers"]');
+  var $cartList = getElement('[class="cart-list"]');
+
 
   function getElement(attributes){
     return document.querySelector(attributes);
@@ -28,6 +30,7 @@
         initialGameSelect();
       }
     }
+    createEventForButtonOptions();
   }
 
   function createGamesListButtons() {
@@ -67,24 +70,26 @@
     buttons.forEach(button => {
       button.addEventListener('click', (event) => {
         event.preventDefault();
-        console.log(listOfNumbers);
+       if(listOfNumbers.length >= selectedGame[0]['max-number']){
+          return console.log('error')
+       }
 
-        if(button.getAttribute('number-option-is-selected') === 'true'){
-          button.setAttribute('number-option-is-selected', 'false');
+        if(listOfNumbers.indexOf(String(button.innerHTML)) !== -1){
           button.style.background = '#ADC0C4';
           var numberIndex = listOfNumbers.indexOf(button.innerHTML);
-          listOfNumbers.slice(toString(numberIndex), 1);
+          listOfNumbers.splice(numberIndex, 1);
         }
         else {
-        button.setAttribute('number-option-is-selected', 'true');
-        button.style.background = '#27C383';
-        listOfNumbers.push(event.target.innerHTML);
-        }
+          button.setAttribute('number-option-is-selected', 'true');
+          button.style.background = '#27C383';
+          listOfNumbers.push(button.innerHTML);
+        }        
       })
     })
   }
 
   function resetDataButtons(){
+    listOfNumbers = [];
     var $buttonSelectedGame = document.querySelectorAll('[class="game-choose-button"]');
     $buttonSelectedGame.forEach(button => {
       var game = listGames[0].filter(game => game.type === button.getAttribute('game-type'));
@@ -92,7 +97,14 @@
       button.style.color = game[0].color;
       button.style.background = '#FFFF'
     })
+    clearSelectedNumbers();    
+  }
 
+  function clearSelectedNumbers () {
+    var $numbersSelected = document.querySelectorAll('[class="number-option"]');
+    $numbersSelected.forEach(number => {
+      number.style.background = "#ADC0C4";
+    })
   }
 
   function setSelectedGame(gameName){
@@ -106,11 +118,6 @@
     $gameDescription.innerHTML = selectedGame[0].description;
   }
 
-  function formatNumberOfButtons(number){
-    var formated = number < 10 ? `0${number}` : number;
-    return formated;
-  }
-
   function createNumbersForGame() {
     $divNumberGame.innerHTML = ''
     for (var i = 1; i <=selectedGame[0].range ; i++) {
@@ -119,6 +126,7 @@
       newButton.appendChild(newButtonText);
       
       newButton.setAttribute('class', 'number-option');
+      newButton.setAttribute('value', formatNumberOfButtons(i));
       newButton.setAttribute('number-option-is-selected', 'false');
       
       $divNumberGame.appendChild(newButton);
@@ -126,8 +134,97 @@
     var $allNumberButtons = document.querySelectorAll('[class="number-option"]');
     createEventForNumberButton($allNumberButtons);
   }
+  
+  function formatNumberOfButtons(number){
+    var formated = number < 10 ? `0${number}` : number;
+    return formated;
+  }
 
+  function generateRandomNumbers(numberMax) {
+    return String(formatNumberOfButtons(Math.ceil(Math.random() * numberMax)));
+  }
 
+  function completeRandomNumers() {
+    listOfNumbers = [];
+    var range =  selectedGame[0].range;
+    while (listOfNumbers.length < selectedGame[0]['max-number']) {
+      var randomNumber = String(generateRandomNumbers(range));
+      var $numberButton = document.querySelector('[value="'+randomNumber+'"]');
+      $numberButton.click();
+    }
+  }
+
+  function createEventForButtonOptions (){  
+    // EVENT TO CLEAR GAME BUTTON  
+    getElement('[class="game-options-clear"]')
+      .addEventListener('click', () => {
+        clearSelectedNumbers();
+    })
+    
+    // EVENT TO ADD TO CART BUTTON
+    getElement('[class="games-add-cart-button"]')
+      .addEventListener('click', () => {
+        addGameToCart();
+        resetDataButtons();
+        initialGameSelect();
+    })
+
+    // EVENT TO COMPLETE RANDOM GAME
+    getElement('[class="game-options-complete"]')
+      .addEventListener('click', () => {
+        completeRandomNumers();
+      })
+  }
+
+  function addGameToCart(){
+    const newGame = {
+      id: String(new Date().getTime()),
+      gameType: selectedGame[0].type,
+      price: selectedGame[0].price,
+      numbers: listOfNumbers,
+      color: selectedGame[0].color
+    }
+
+    if(newGame.numbers.length < selectedGame[0]['max-number']){
+      return console.log('numeros inferiores');
+    }
+    cart.push(newGame);
+    createGameOnCart(newGame);
+    console.log(cart);
+  }
+
+  function createGameOnCart(game) {
+    var gameCartDiv = document.createElement('div');
+    var gameCartInfosDiv = document.createElement('div');
+    var gameCartNumbers = document.createElement('p');
+    var gameCartType = document.createElement('p');
+    var gameCartPrice = document.createElement('span');
+    var gameCartRemoveButton = document.createElement('button');
+
+    gameCartDiv.setAttribute('class', 'game-cart-div');
+    gameCartInfosDiv.setAttribute('class', 'game-cart-info');
+
+    gameCartNumbers.setAttribute('class', 'game-cart-numbers');
+    gameCartType.setAttribute('class', 'game-cart-type');
+    gameCartPrice.setAttribute('class', 'game-cart-price');
+
+    gameCartRemoveButton.setAttribute('class', 'game-cart-remove');
+    gameCartRemoveButton.setAttribute('game-id', game.id);
+
+    gameCartNumbers.textContent = game.numbers.join(', ');
+    gameCartType.textContent = game.gameType;
+    gameCartPrice.textContent = game.price;
+
+    gameCartInfosDiv.style.borderLeft = `3px ${game.color} solid`;
+    gameCartInfosDiv.appendChild(gameCartNumbers);
+    gameCartInfosDiv.appendChild(gameCartType);
+    gameCartInfosDiv.appendChild(gameCartPrice);
+
+    gameCartDiv.appendChild(gameCartRemoveButton);
+    gameCartDiv.appendChild(gameCartInfosDiv);
+
+    return $cartList.appendChild(gameCartDiv);
+  }
 
   getAllGames();
 })(window, document);
